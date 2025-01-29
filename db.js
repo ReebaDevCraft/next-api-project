0c86f43ce0f81d222eebd0f8e6ce733b006c491a
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-01-29 21:27:56 +0500

    Add files via upload
    Next.js API project with a connected database, designed to handle dynamic data requests. This project includes sample data to demonstrate the API's functionality, providing a robust foundation for building data-driven applications. It supports seamless integration with the database for CRUD operations and scalable data handling

---
+++ db.js
@@ -0,0 +1,27 @@
+export const user = [
+    {
+      name: "Jhon",
+      id: "1",
+      age: 23,
+      email: "johndoe@example.com",
+    },
+    {
+      name: "Hary",
+      id: "2",
+      age: 25,
+      email: "hary@example.com",
+    },
+    {
+      name: "Petter",
+      id: "3",
+      age: 30,
+      email: "pettere@example.com",
+    },
+    {
+      name: "Fari",
+      id: "4",
+      age: 5,
+      email: "farie@example.com",
+    },
+  ];
+  
